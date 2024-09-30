c783902eedadd501f6e7d25f17ac371c8b6fe3ca
PraeThipwarin <prae.tippy@gmail.com>
2024-09-30 16:21:59 +0700

    finish1

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,18 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
+interface CommentProps {
+  userImagePath: string,
+  username: string,
+  commentText: string,
+  likeNum: number,
+  replies: ReplyProps[],
+}
+export type { CommentProps };
 
-interface ReplyProps {}
+interface ReplyProps {
+    userImagePath: string, 
+    username: string, 
+    replyText: string, 
+    likeNum: number,
+}
 export type { ReplyProps };
